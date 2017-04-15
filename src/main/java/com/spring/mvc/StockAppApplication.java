0dd9742528e63664c0fa6556cbe6f393c733d8fd
dyngosz <jestemwiciu@gmail.com>
2017-04-15 18:04:06 +0200

    Added spring specific annotations

--- src/main/java/com/spring/mvc/StockAppApplication.java
+++ src/main/java/com/spring/mvc/StockAppApplication.java
@@ -1,9 +1,13 @@
 package com.spring.mvc;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan({"com.spring.domain", "com.spring.service", "com.spring.web"})
 public class StockAppApplication {
 
 	public static void main(String[] args) {
