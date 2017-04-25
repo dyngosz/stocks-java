df4b56c3c64336bba15b166e4eb596e0a3e46923
dyngosz <jestemwiciu@gmail.com>
2017-04-25 22:29:50 +0200

    Implementing navbar search for stock ID

--- src/main/resources/static/app/js/directives.js
+++ src/main/resources/static/app/js/directives.js
@@ -3,5 +3,11 @@
     'use strict';
 
     var App = angular.module('App');
+    App.directive('navbarMenu', [function () {
 
+        return {
+            controller: 'NavBarController',
+            templateUrl: 'app/views/navbar-menu.html'
+        };
+    }]);
 })(window.angular);
